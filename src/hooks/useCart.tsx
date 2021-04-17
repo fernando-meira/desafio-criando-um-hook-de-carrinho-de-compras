4cc3c230d11a4b65b68dfe402492be825bad5097
fernando-meira <thomwork@outlook.com>
2021-04-17 00:13:53 -0300

    feat: 🎸 Added items in Home and created function to add product

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,49 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  console.log("cart", cart)
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const { data } = await api.get<UpdateProductAmount>(`stock/${productId}`);
+
+      const currentCart = [...cart];
+      const stockAmount = data.amount;
+      const productExist = currentCart.find(product => product.id === productId);
+      const currentAmount = productExist ? productExist.amount : 0;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if (productExist) {
+        productExist.amount = amount;
+      } else {
+        const { data } = await api.get(`products/${productId}`)
+
+        const newProduct = {
+          ...data,
+          amount: 1
+        }
+
+        currentCart.push(newProduct);
+      }
+
+      setCart(currentCart);
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(currentCart));
     } catch {
-      // TODO
+      toast.error('Quantidade solicitada fora de estoque');
     }
   };
 
